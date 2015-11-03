6cee0d2b93b66986044469c3998c3c95b89b8d34
Kessler <kessler.dev@gmail.com>
2015-11-03 18:33:16 -0500

    Applied fixes from StyleCI

--- src/Providers/Traits/CommandLoaderTrait.php
+++ src/Providers/Traits/CommandLoaderTrait.php
@@ -1,12 +1,11 @@
-<?php namespace Skimia\Foundation\Providers\Traits;
+<?php
 
+namespace Skimia\Foundation\Providers\Traits;
 
 use Illuminate\Support\ServiceProvider;
 
 trait CommandLoaderTrait
 {
-
-
     /**
      * Register the commands.
      *
@@ -16,8 +15,7 @@ trait CommandLoaderTrait
     {
         ensure_trait_used_in_subclass_of_or_fail(self::class, $this, ServiceProvider::class);
 
-        foreach (array_keys($this->commands) as $command)
-        {
+        foreach (array_keys($this->commands) as $command) {
             $method = "register{$command}Command";
             call_user_func_array([$this, $method], []);
         }
--- tests/Providers/Traits/fixtures/ServiceProviderMock.php
+++ tests/Providers/Traits/fixtures/ServiceProviderMock.php
@@ -5,42 +5,37 @@ class ServiceProviderMock extends \Illuminate\Support\ServiceProvider
 {
     use \Skimia\Foundation\Providers\Traits\CommandLoaderTrait;
 
-
-
     protected $commands = [
         'Test' => 'skimia.phpunit.command.test',
     ];
-    
+
     /**
-     * @inheritDoc
+     * {@inheritdoc}
      */
     public function register()
     {
         $this->registerCommands();
     }
 
-    public function removeCommandsField(){
-        unset ($this->commands);
+    public function removeCommandsField()
+    {
+        unset($this->commands);
     }
-
 
     public $registerCommandIsFired = false;
 
-    public function registerTestCommand(){
-
+    public function registerTestCommand()
+    {
         $this->registerCommandIsFired = true;
 
-        $this->app->singleton('skimia.phpunit.command.test', function ($app)
-        {
+        $this->app->singleton('skimia.phpunit.command.test', function ($app) {
             //TODO: return a real instance of command;
             return new CommandMock();
         });
     }
-
 }
 
-class CommandMock extends \Illuminate\Console\Command{
-
+class CommandMock extends \Illuminate\Console\Command
+{
     protected $signature = 'skimia.phpunit.command.test';
-
 }
