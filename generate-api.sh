609f81d4511fd6e6c7170063dc491f4b944ee6b4
Kessler Dev <kessler.dev@gmail.com>
2015-11-14 00:37:28 +0100

    maj infos

--- generate-api.sh
+++ generate-api.sh
@@ -2,8 +2,9 @@
 wget http://www.apigen.org/apigen.phar
 
 # Generate Api
-php apigen.phar generate -s src -d ../gh-pages/api
-cd ../gh-pages
+mkdir gh-pages
+php apigen.phar generate -s src -d gh-pages/api
+cd gh-pages
 
 # Set identity
 git config --global user.email "travis@travis-ci.org"
